158deb9d3ae1a014a3184057c15cd13e1f5ca4f2
Bilan Claudiu <38669310+bilan-claudiu@users.noreply.github.com>
2022-02-19 15:27:57 +0200

    add total for budget

---
+++ app/src/main/java/com/example/pbt/Data.java
@@ -0,0 +1,67 @@
+package com.example.pbt;
+
+public class Data {
+
+    String item, date, id, notes;
+    int amount, mounth;
+
+    public Data() {
+    }
+
+    public Data(String item, String date, String id, String notes, int amount, int mounth) {
+        this.item = item;
+        this.date = date;
+        this.id = id;
+        this.notes = notes;
+        this.amount = amount;
+        this.mounth = mounth;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getMounth() {
+        return mounth;
+    }
+
+    public void setMounth(int mounth) {
+        this.mounth = mounth;
+    }
+}

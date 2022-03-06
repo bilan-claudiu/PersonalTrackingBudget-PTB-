63605080bccf505c7ca1be7d9769d26bb2b10681
Bilan Claudiu <38669310+bilan-claudiu@users.noreply.github.com>
2022-03-06 14:12:52 +0200

    update history

---
+++ app/src/main/java/com/example/pbt/DailyAnalyticsActivity.java
@@ -0,0 +1,14 @@
+package com.example.pbt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_analytics);
+    }
+}

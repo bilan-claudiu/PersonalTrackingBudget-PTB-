f0d55d01d0ead86968a1e5188bb3febd5829dc1f
Bilan Claudiu <bilan.claudiu@gmail.com>
2022-03-15 14:40:26 +0200

    implemented 'today analytics' feature

--- app/src/main/java/com/example/pbt/ChooseAnalyticsActivity.java
+++ app/src/main/java/com/example/pbt/ChooseAnalyticsActivity.java
@@ -1,14 +1,29 @@
 package com.example.pbt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class ChooseAnalyticsActivity extends AppCompatActivity {
+    private CardView todayCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_analytics);
+
+        todayCardView = findViewById(R.id.todayCardView);
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseAnalyticsActivity.this, DailyAnalyticsActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
